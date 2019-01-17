e9fad83f29803bbbd2c0f43a928f8870101444c8
songshuran <songshuran@shouqianba.com>
2019-01-17 13:30:17 +0800

    初始化

---
+++ src/main/java/com/ooqiu/gaming/server/dependencies/GamingServerDependenciesApplication.java
@@ -0,0 +1,14 @@
+package com.ooqiu.gaming.server.dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingServerDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingServerDependenciesApplication.class, args);
+    }
+
+}
+
